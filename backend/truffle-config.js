2e3e91bd8ea09a69dc475b9927e0cb663143c622
ZoomerDegen <augustusflick@gmail.com>
2024-07-29 12:03:04 +0200

    Update truffle-config.js

--- backend/truffle-config.js
+++ backend/truffle-config.js
@@ -3,12 +3,12 @@ module.exports = {
     development: {
       host: "127.0.0.1", // Localhost (default: none)
       port: 7545,        // Standard Ganache port
-      network_id: "*",   // Any network (default: none)
+      network_id: "534352",   // Scroll network ID
     },
-    // Other network configurations can go here
+
   },
 
-  // Set default mocha options here, use special reporters, etc.
+
   mocha: {
     // timeout: 100000
   },
@@ -16,9 +16,9 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.8.21",      // Fetch exact version from solc-bin (default: truffle's version)
-      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
+      version: "0.8.19",      
+      // docker: true,        
+      // settings: {          
       //  optimizer: {
       //    enabled: false,
       //    runs: 200
@@ -27,26 +27,5 @@ module.exports = {
       // }
     }
   },
-
-  // Truffle DB is currently disabled by default; to enable it, change enabled:
-  // false to enabled: true. The default storage location can also be
-  // overridden by specifying the adapter settings, as shown in the commented code below.
-  //
-  // NOTE: It is not possible to migrate your contracts to truffle DB and you should
-  // make a backup of your artifacts to a safe location before enabling this feature.
-  //
-  // After you backed up your artifacts you can utilize db by running migrate as follows:
-  // $ truffle migrate --reset --compile-all
-  //
-  // db: {
-  //   enabled: false,
-  //   host: "127.0.0.1",
-  //   adapter: {
-  //     name: "indexeddb",
-  //     settings: {
-  //       directory: ".db"
-  //     }
-  //   }
-  // }
 };
 
